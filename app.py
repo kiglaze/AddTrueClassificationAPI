5520dd5cbca91da6b2449028883dcc63c3a7452f
kiglaze <kiglaze@ncsu.edu>
2025-11-21 01:59:53 -0500

    using .env file for locating main project directory, and pulling text data as well.

--- app.py
+++ app.py
@@ -1,26 +1,31 @@
 from flask import Flask, send_from_directory, request
 import sqlite3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
-DATABASE = "extracted_texts.db"
-
 # Absolute path to your saved_images directory
-SAVED_IMAGES_DIR = '/Users/irisglaze/code/thesis/MitmProxyAdPull/saved_images'
+MITMPROXY_AD_PULL_PROJECT_DIR = os.getenv('MITMPROXY_AD_PULL_PROJECT_DIR')
+SAVED_IMAGES_DIR = os.path.join(MITMPROXY_AD_PULL_PROJECT_DIR.rstrip('/'), 'saved_images')
+
+DATABASE_FILEPATH = os.path.join(MITMPROXY_AD_PULL_PROJECT_DIR.rstrip('/'), "extracted_texts.db")
 
 @app.route('/saved_images/<path:filename>')
 def serve_saved_image(filename):
     return send_from_directory(SAVED_IMAGES_DIR, filename)
 
 def get_db_connection():
-    conn = sqlite3.connect(DATABASE)
+    conn = sqlite3.connect(DATABASE_FILEPATH)
     conn.row_factory = sqlite3.Row  # makes rows behave like dictionaries
     return conn
 
 @app.route('/')
 def get_unclassified_imgs_w_text_data():
     conn = get_db_connection()
-    rows = conn.execute("SELECT it.id AS id, it.full_filepath, is_suspected_ad_manual FROM image_texts it LEFT JOIN image_saved_data isd ON isd.full_filepath = it.full_filepath WHERE is_suspected_ad_manual IS NULL ORDER BY RANDOM()").fetchall()
+    rows = conn.execute("SELECT it.id AS id, it.full_filepath, text, text_script, is_suspected_ad_manual FROM image_texts it LEFT JOIN image_saved_data isd ON isd.full_filepath = it.full_filepath WHERE is_suspected_ad_manual IS NULL ORDER BY RANDOM()").fetchall()
     conn.close()
 
     # convert query results into a string or JSON
